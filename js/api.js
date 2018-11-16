3c9ab7d50c00bb3d11ba404a0ab162941f11f31b
Neha Sadhvi <iam@nehasadhvi.com>
2018-11-16 05:23:02 -0800

    Displaying results on the screen

---
+++ js/api.js
@@ -0,0 +1,12 @@
+export class API {
+    constructor(artist, song){
+        this.artist = artist;
+        this.song = song;
+    }
+
+    async queryAPI() {
+        const url = await fetch(`https://api.lyrics.ovh/v1/${this.artist}/${this.song}`);
+        const lyrics = await url.json();
+        return lyrics;
+    }
+}
